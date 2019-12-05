6a2e99622032cb4891341531ba7d516b681ea70e
Vinnys Lins <vinnysflins@gmail.com>
2019-12-05 18:00:18 -0300

    first commit

---
+++ install/zsh.sh
@@ -0,0 +1,8 @@
+# zsh
+sudo apt install zsh
+
+# oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# zplugin
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
