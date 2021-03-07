5ded309f30a9d52375cdee46bffafacb269d22dc
Vinnys Lins <vinnysflins@gmail.com>
2021-03-07 16:13:50 -0300

    update zinit installer

--- install/zsh.sh
+++ install/zsh.sh
@@ -4,5 +4,5 @@ sudo apt install zsh
 # oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-# zplugin
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+# zinit
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
