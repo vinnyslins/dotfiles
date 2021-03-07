b6f694339d7bd031f765939db5df3c13c6041d7d
Vinnys Lins <vinnysflins@gmail.com>
2021-03-07 16:14:17 -0300

    update install scripts

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,2 +1,1 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
